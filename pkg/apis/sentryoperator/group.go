686eadb89d9270f5111e9e5040fd4841480b13ea
Mark Turansky <mturansk@redhat.com>
2019-08-01 17:13:07 -0400

    removed the old stuff

--- pkg/apis/sentryoperator/group.go
+++
@@ -1,6 +0,0 @@
-// Package sentryoperator contains sentryoperator API versions.
-//
-// This file ensures Go source parsers acknowledge the sentryoperator package
-// and any child packages. It can be removed if any other Go source files are
-// added to this package.
-package sentryoperator
